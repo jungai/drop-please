df94379a7dd94bb0850157267ba438391daa6b11
jungai <juworaphol125@gmail.com>
2021-08-29 14:17:06 +0700

    add build images script

--- scripts/build.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zx
-
-(async () => {
-  const { name } = await fs.readJSON(__dirname + "/../package.json");
-
-  await $`echo ${name}`;
-})();
---
+++ scripts/build_images.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env zx
+
+(async () => {
+  const { name, version } = await fs.readJSON(__dirname + "/../package.json");
+  const imageName = `${name}:${version}`;
+  const platform = ["linux/amd64", "linux/arm64"].forEach(async (arch) => {
+    await $`docker buildx build --platform ${arch} -t ${imageName}-${
+      arch.split("/")[1]
+    } .`;
+  });
+})();
