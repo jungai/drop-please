8471eb325a3edbea91290c710cf240ccadc3ccf5
jungai <juworaphol125@gmail.com>
2021-08-29 14:18:05 +0700

    update script

--- scripts/build_images.mjs
+++ scripts/build_images.mjs
@@ -3,7 +3,9 @@
 (async () => {
   const { name, version } = await fs.readJSON(__dirname + "/../package.json");
   const imageName = `${name}:${version}`;
-  const platform = ["linux/amd64", "linux/arm64"].forEach(async (arch) => {
+  const platform = ["linux/amd64", "linux/arm64"];
+
+  platform.forEach(async (arch) => {
     await $`docker buildx build --platform ${arch} -t ${imageName}-${
       arch.split("/")[1]
     } .`;
